51b5b258b8e4096a54ed418a5c35c559597f3819
ethan <epollack31@gmail.com>
2024-02-07 15:46:01 -0800

    Started Project

---
+++ Assets/Scripts/SnakeHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a58b478f1cd9f91863a277ad68454fadd68b8bb92a41150f93e92a598fb675f
+size 1816
---
+++ Assets/Scripts/SnakePartHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:573371fa5c0100def5a7ca4750838de703d2b8c286ac160cc95d26b0e4406fc5
+size 473
