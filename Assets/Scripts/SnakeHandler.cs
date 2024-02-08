9ebb0412149e597a200a1c9e38a7ba10bfca8320
ethan <epollack31@gmail.com>
2024-02-07 16:14:17 -0800

    added grow button

--- Assets/Scripts/SnakeHandler.cs
+++ Assets/Scripts/SnakeHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:123ec16296e54ec465dbfaf64481d34ffa0222f81010ad5dc8a16fce18562fbb
-size 1861
+oid sha256:94e0d1d9a892866386594aad41fb2b5ea05d0f064852bd66701c05343717d059
+size 1930
