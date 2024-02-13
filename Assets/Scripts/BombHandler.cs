5e47425b020e9cc7c1077d80f8d4d6d0d892e02d
ethan <epollack31@gmail.com>
2024-02-13 09:34:37 -0800

    food and bomb spawn avoiding specified colliders / random food and bomb scales on spawn

--- Assets/Scripts/BombHandler.cs
+++ Assets/Scripts/BombHandler.cs
@@ -2,16 +2,53 @@ using UnityEngine;
 
 public class BombHandler : MonoBehaviour
 {
-    public GameObject BombPrefab;
+  public GameObject BombPrefab;
 
-    void Start() {
+  void Start()
+  {
 
-        AddBomb();
-        AddBomb();
+    AddBomb();
+    AddBomb();
+  }
+
+
+  private Vector2 GetNewBombPosition(GameObject bomb)
+  {
+    Vector2 position = StaticHelpers.RandomScreenPosition(0.1f, 0.1f);
+    Bounds bounds = bomb.GetComponent<Collider2D>().bounds;
+    bounds.center = position;
+    bounds.Expand(0.5f);
+
+
+    GameObject snakeHead = GameObject.FindGameObjectWithTag("Snake");
+    GameObject[] snakeParts = GameObject.FindGameObjectsWithTag("SnakePart");
+    GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
+
+
+    GameObject[] all = new GameObject[snakeParts.Length + bombs.Length + 1];
+    all[0] = snakeHead;
+    snakeParts.CopyTo(all, 1);
+    bombs.CopyTo(all, snakeParts.Length + 1);
+
+    for (int i = 0; i < all.Length; i++)
+    {
+      if (bounds.Intersects(all[i].GetComponent<Collider2D>().bounds))
+      {
+        return GetNewBombPosition(bomb);
+      }
     }
 
-    public void AddBomb() {
-        GameObject bomb = Instantiate(BombPrefab, transform);
-        bomb.transform.SetPositionAndRotation(StaticHelpers.RandomScreenPosition(0.1f, 0.1f), StaticHelpers.RandomRotation());
-    }
+    return position;
+  }
+
+  public void AddBomb()
+  {
+    Collider2D[] colliders = StaticHelpers.GetComponentsFromSnakeParts<Collider2D>();
+    GameObject bomb = Instantiate(BombPrefab, transform);
+
+    Vector2 position = StaticHelpers.RandomPositionAvoidingColliders(new Vector2(0.1f, 0.1f), bomb.GetComponent<Collider2D>(), colliders);
+   
+    bomb.transform.localScale = StaticHelpers.GetRandomScale(0.1f, 0.15f); 
+    bomb.transform.SetPositionAndRotation(position, StaticHelpers.RandomRotation());
+  }
 }
--- Assets/Scripts/StaticHelpers.cs
+++ Assets/Scripts/StaticHelpers.cs
@@ -13,9 +13,43 @@ public static class StaticHelpers
     return Camera.main.ViewportToWorldPoint(new Vector2(marginX + Random.value * (1 - marginX * 2), marginY + Random.value * (1 - marginY * 2)));
   }
 
-
   public static Quaternion RandomRotation() {
     float angle = Random.Range(minInclusive: -180f, 180f);
     return Quaternion.Euler(0, 0, angle);
+  }
+
+  public static Vector2 RandomPositionAvoidingColliders(Vector2 margin, Collider2D collider, Collider2D[] others) {
+    Vector2 position = RandomScreenPosition(margin);
+    Bounds bounds = collider.bounds;
+    bounds.center = position;
+    bounds.Expand(1f);
+
+    for (int i = 0; i < others.Length; i++) {
+      if (others[i] == null) {
+        continue;
+      }
+
+      if (bounds.Intersects(others[i].bounds)) {
+        return RandomPositionAvoidingColliders(margin, collider, others);
+      }
+    }
+
+    return position;
+  }
+
+  public static T[] GetComponentsFromSnakeParts<T>() {
+    GameObject snakeHead = GameObject.FindGameObjectWithTag("Snake");
+    GameObject[] snakeParts = GameObject.FindGameObjectsWithTag("SnakePart");
+    T[] components = new T[snakeParts.Length + 1];
+    components[0] = snakeHead.GetComponent<T>();
+    for (int i = 0; i < snakeParts.Length; i++) {
+      components[i + 1] = snakeParts[i].GetComponent<T>();
+    }
+    return components;
+  }
+
+  public static Vector3 GetRandomScale(float min = 0.75f, float max = 2f) {
+    float scale = Random.Range(min, max);
+    return new Vector3(scale, scale, 1);
   }
 }
