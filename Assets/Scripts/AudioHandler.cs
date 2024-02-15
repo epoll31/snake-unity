9969cca8010adeb578fd2a30379eee8e9ecc2d34
ethan <epollack31@gmail.com>
2024-02-15 11:19:39 -0800

    renamed Assets -> Images / moved Assets/Fonts -> Fonts / added Sounds / added AudioHandler script / added Sounds array, enum, and class to SingleState / added PlayClip function to SingleState

---
+++ Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6102b3f58c2aa7f3e06425dd49017e4d1e5101092c4cf7e6c2c81af5cfa2feaf
+size 165
--- Assets/Scripts/SingleState.cs
+++ Assets/Scripts/SingleState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:611a0ef11aa03eb212abf4b8a3a6d87cf437458983d499bb8f4466806d4809a7
-size 4020
+oid sha256:4a08e7d04abcdb620696fffeaee9e02eb97e615274ae29b1e72d384e68eca95a
+size 4134
