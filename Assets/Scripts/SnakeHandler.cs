20e983f81e6946c391cf3d19567d3301f5e7f727
ethan <epollack31@gmail.com>
2024-02-16 09:13:31 -0800

    added lering to angle in snake / only lerps when touch is outside of 2 lengthPerParts

--- Assets/Scripts/SnakeHandler.cs
+++ Assets/Scripts/SnakeHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d66c3e0ed60f5cf509dd77e912152859d306d6131b758db3e83a241d5b1281d
-size 3334
+oid sha256:33b5ecd71e8307f6a75f8d5354acf8a9cb91048249b4ac967272610fa2ed4c91
+size 3697
